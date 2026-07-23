65a713dc465e021dec3d6f971a084cef7c56d630
Aashita Mishra <ashita.mofficial@gmail.com>
2026-07-23 16:32:47 +0530

    Add solution for LeetCode 142

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -0,0 +1,43 @@
+// Last updated: 7/23/2026, 3:57:41 PM
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+
+    public ListNode detectCycle(ListNode head) {
+
+        if (head == null)
+            return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+
+                ListNode ptr = head;
+
+                while (ptr != slow) {
+                    ptr = ptr.next;
+                    slow = slow.next;
+                }
+
+                return ptr;
+            }
+        }
+
+        return null;
+    }
+}
--- 946-validate-stack-sequences/Solution.java
+++ 946-validate-stack-sequences/Solution.java
@@ -1,17 +1,32 @@
+import java.util.*;
+
 class Solution {
+
     public boolean validateStackSequences(int[] pushed, int[] popped) {
+
         Stack<Integer> stack = new Stack<>();
         int j = 0;
-        
-            for ( int num : pushed){
-            
-                stack.push(num);
-                while (!stack.isEmpty() && stack.peek() == popped[j] ){
-                    stack.pop();
-                    j++;
+
+        for (int num : pushed) {
+
+            stack.push(num);
+
+            while (!stack.isEmpty() && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
         }
-        
+
+        return stack.isEmpty();
     }
-    return stack.isEmpty();
+
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        int[] pushed = {1,2,3,4,5};
+        int[] popped = {4,5,3,2,1};
+
+        System.out.println(obj.validateStackSequences(pushed, popped));
+    }
 }
-};
